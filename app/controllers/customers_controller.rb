3b1a9745d2ed2a36311ca95b3f4a546f38c90c7d
nienkebos <nienkebos@live.com>
2016-08-17 21:23:26 +0200

    namefield on sign up form

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,33 +1,39 @@
 class CustomersController < ApplicationController
-    before_action :set_customer, only: [:show, :edit, :update, :destroy]
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
-    def index
+  def index
       @customers = Customer.all
-    end
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def new
+  def new
       @customer = Customer.new
-    end
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    def create
-      @customer = Customer.new(user_params)
+  def create
+    @customer = Customer.new(user_params)
 
-      respond_to do |format|
-        if @customer.save
-          format.html { redirect_to @customer, notice: 'User was successfully created.' }
-          format.json { render :show, status: :created, location: @customer }
-        else
-          format.html { render :new }
-          format.json { render json: @customer.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @customer.save
+        format.html { redirect_to @customer, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @customer }
+      else
+        format.html { render :new }
+        format.json { render json: @customer.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-    
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :password)
+  end
+
+
 end
